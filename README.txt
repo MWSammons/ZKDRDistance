2bde288a3a168d15ebe1ee7c3df094b660cb5e85
MWSammons <82555500+MWSammons@users.noreply.github.com>
2021-04-15 14:27:17 +0800

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,81 @@
+ZKDR Distance equation implemented in Python 
+    - Author: Mawson William Sammons (mawson.sammons@postgrad.curtin.edu.au)
+
+  
+This function set provides a Python implementation of the ZKDR distance equation for both 
+the model derived by Kayser 1997 in "General and Practical Method for Calculating Cosmological
+distances" and Linder 1988 in "Light Propagation in Generalized Friedmann Universes". 
+Functionally this code calculates the angular diameter distance at redshift z of a source 
+in a universe where a fraction (eta) of the matter is distributed smoothly and the remainder is
+in compact clumps far from the line of sight. For details on the motivation and application
+of this distance measure see the accompanying paper (PAPER LINK). 
+
+/////////////////
+USE and Examples:
+/////////////////
+
+This function set has been designed to integrate with existing AstroPy cosmology functions and
+requires the user to create their own cosmology object with their preferred parameters.
+
+Once a cosmology object has been generated the functions contained in ZKDRDistance.py have been
+imported the ZKDR angular diameter distance can be calculated by calling the relevant function
+as shown below:
+
+
+----Python 3.0 code------
+from astropy.cosmology import Planck18 as cosmo
+import ZKDRDistance as ZKDR
+
+z1=0      #Start redshift
+z2=1.0    #End redshift
+eta=1.0   #fraction of matter which is smoothly distributed
+method=0  #method to use (1)=Kayser,   (2)=Linder
+
+D_eta=ZKDR.angular_diameter_distance_ZKDR(z1, z2, eta, method, cosmo)
+#Answer should be 1698.8580811 Mpc
+-------------------------
+
+
+This gives the angular diameter distance for an observer at redshift zero in a universe where 
+matter is distributed completely homogeneously. The angular diameter distance to a fictional 
+observer at some non-zero redshift z1 (often required in gravitational lensing calculations)
+can be calculated just as above by having a non-zero z1 value.
+
+The answer calculated above is close (relative error ~ 1/1000) to the analytic angular diameter
+distance at redshift of 1 for a homogeneous universe; 1697.81723558 Mpc (as given by 
+cosmo.angular_diameter_distance(z2)). As mentioned in the accompanying paper this discrepancy 
+arises due to cosmo.Om0+cosmo.Ode0 < 1 in the Planck model; the remaining density, which is in 
+relativisitic matter and radiation is instead interpretted as curvature in the Kayser model, 
+yielding the slightly different value. By forcing the Planck cosmology to be flat this error 
+can be reduced, this is done by aggregating relativistic matter and radiation densities into 
+the cold matter density parameter, as shown below:
+
+
+------------------------
+from astropy.cosmology import LambdaCDM
+cosmo2=lambdaCDM(H0=cosmo.H0, Om0=1-cosmo.Ode0, Ode0=cosmo.Ode0)
+D_etaForcedFlat=ZKDR.angular_diameter_distance_ZKDR(z1, z2, eta, method, cosmo2)
+#Answer should be 1697.88049949 Mpc
+----------------------- 
+
+
+This answer is much closer (relative error ~1/10000).
+
+Alternatively we can make use of Linder's generalised method which treats both relativisitic
+matter and radiation densities directly.
+
+
+-----------------------
+method=1
+D_etaLinder=ZKDR.angular_diameter_distance_ZKDR(z1, z2, eta, method, cosmo)
+#Answer should be 1696.85930469 Mpc
+-----------------------
+
+
+However the answer has a similar relative error as the first attempt (relative error ~ 1/1000).
+
+
+
+
+
+
---
+++ ZKDRDistance.py
@@ -0,0 +1,95 @@
+import numpy as np
+import astropy.units as u
+from astropy import constants as const
+import scipy.integrate
+import scipy.special
+
+
+def DyerRoeder(z, Dset, eta, cosmo):
+    """THIS FUNCTION IS A COMPONENT OF angular_diameter_distance_ZKDR FUNCTION
+        
+       the 2nd order differential equation to be solved for the ZKDR distance as described in 
+       Kayser 1997, A General and Practical Method for Calculating Cosmological Distances
+       z= cosmological redshift
+       Dset = 1D array containing the current values of D and Dprime (differentiated W.R.T. 
+       redshift)
+       eta = the fraction of matter that is smooth
+       Omega0 = the density parameter for matter at redshift z=0 (now)
+       Lambda0 = the cosmological constant at z=0
+       cosmo = AstroPy cosmology object containing the users preferred cosmological parameters
+       FOR USE WITH SOLVE_IVP
+    """
+    Omega0=cosmo.Om0
+    Lambda0=cosmo.Ode0
+    D, Dprime = Dset
+    Qval=Q(z, Omega0, Lambda0)
+    Qprimeval=Qprime(z, Omega0, Lambda0)
+    temp=(-1*(2*Qval/(1+z)+1/2*Qprimeval)*Dprime-3/2*eta*Omega0*(1+z)*D)/Qval
+    dDsetdz=[Dprime, temp]
+    return dDsetdz
+
+def Qprime(z, Omega0, Lambda0): 
+    """THIS FUNCTION IS A COMPONENT OF THE DyerRoeder FUNCTION
+ 
+       Q parameter from Kayser 1997 differentiated w.r.t. z
+    """
+    return 3*Omega0*(1+z)**2-2*(Omega0+Lambda0-1)*(1+z) 
+
+def Q(z, Omega0, Lambda0):
+    """THIS FUNCTION IS A COMPONENT OF THE DyerRoeder FUNCTION
+       Q parameter from Kayser 1997
+    """
+    return Omega0*(1+z)**3-(Omega0+Lambda0-1)*(1+z)**2+Lambda0
+
+def GeneralisedDyerRoeder(z, Dset, eta, cosmo):
+    """THIS FUNCTION IS A COMPONENT OF angular_diameter_distance_ZKDR
+       E.V. Linder's generalisation of Dyer-Roeder equation from Light Propagation in 
+       Generalized Friedmann Universes 1988. Generalisation comes in  the form of incorporating
+       a wider range of density parameters, i.e. including relativistic matter Onu and radiation
+       Ogamma
+       z = cosmological redshift
+       Dset = 1D array containing the current values of D and Dprime (differentiated W.R.T. 
+       redshift)
+       eta = the fraction of matter that is smooth
+       cosmo = AstroPy cosmology object containing the users preferred cosmological parameters
+       FOR USE WITH SOLVE_IVP
+    """
+    D, Dprime= Dset
+    temp=-1*((3+decelParam(z, cosmo))/(1+z)*Dprime+3/(2*(1+z)**2)*D*(eta*cosmo.Om(z)+4/3*cosmo.Onu(z)+4/3*cosmo.Ogamma(z)))
+    dDsetdz=[Dprime, temp]
+    return dDsetdz
+
+def decelParam(z, cosmo):
+    """THIS FUNCTION IS A COMPONENT OF THE GeneralisedDyerRoeder FUNCTION
+       Deceleration parameter (q) in E. Linder's generalised DyerRoeder equation from 
+       Light Propagation in Generalized Friedmann Universes, 1988
+    """
+    temp=1/2*(cosmo.Om0*(1+z)+cosmo.Onu0*2*(1+z)**2+cosmo.Ogamma0*2*(1+z)**2-cosmo.Ode0*2*(1+z)**(-2))/(cosmo.Om0*(1+z)+cosmo.Onu0*(1+z)**2+cosmo.Ogamma0*(1+z)**2+cosmo.Ode0*(1+z)**(-2)-cosmo.Ok0)
+    return temp
+
+def angular_diamter_distance_ZKDR(z1, z2, eta, method, cosmo, maxStep=0.01):
+    """Solves the 2nd order D.E. for the ZKDR distance from z1 to z2
+       z1= start redshift
+       z2= end redshift
+       eta = fraction of matter which is smoothly distributed
+       method = which implementation to use, 0=Kayser 1997, 1=Linder 1988 
+                Kayser - Assumes a universe made of cold matter (OmegaM), dark energy (Lambda) 
+                         and some curvature (1-OmegaM-Lambda)
+                Linder - Assumes a generalised universe made of cold matter (OmegaM), dark 
+                         energy (Lambda), relativistic matter (OmegaNu), radiation (OmegaGamma)
+                         and some curvature (K)
+                - NOTE: In this version these parameters will be extracted from the cosmology
+                        object (cosmo.Ox)
+       cosmo = AstroPy cosmology object containing the users preferred cosmological parameters
+    """ 
+    Dset0=[0, cosmo.H0/(cosmo.H(z1)*(1+z1))]
+    if method==0: #select Kayser method 
+        sol=scipy.integrate.solve_ivp(DyerRoeder, [z1,z2], Dset0, args=[eta,cosmo], max_step=maxStep)
+    elif method==1: #select Linder method
+        sol=scipy.integrate.solve_ivp(GeneralisedDyerRoeder, [z1,z2], Dset0, args=[eta,cosmo], max_step=maxStep)
+    else:
+        print('For the method parameter select either 0 : Kayser, or 1 : Linder')
+    return (sol.y[0,len(sol.t)-1]*const.c/cosmo.H0).decompose()/(const.pc*1e6)*u.Mpc 
+        
+        
+    
